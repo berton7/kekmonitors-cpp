30d7dcd0e86e62458360137e9d466e93d931e414
berton7 <francy.berton99@gmail.com>
2021-07-27 14:03:45 +0200

    Updated stopmm.cpp

--- src/bin/stopmm.cpp
+++ src/bin/stopmm.cpp
@@ -1,52 +1,49 @@
 #include <boost/asio.hpp>
 #include <functional>
 #include <iostream>
+#include <kekmonitors/connection.hpp>
 #include <kekmonitors/msg.hpp>
 #include <kekmonitors/utils.hpp>
 
-using namespace boost::asio;
-using namespace kekmonitors;
-
-char buf[1024];
-auto logger = utils::getLogger("Stopmm");
-
-void onRead(const error_code &err, size_t) {
-    if (err && err != error::eof)
-        logger->error(err.message());
-    else
-        logger->info(utils::errorToString(Response::fromString(buf).getError()));
-}
-
-void onWrite(const error_code &err, size_t,
-             local::stream_protocol::socket *socket) {
-    if (err) {
-        logger->error(err.message());
-        return;
-    }
-    socket->shutdown(local::stream_protocol::socket::shutdown_send);
-    async_read(*socket, buffer(buf), &onRead);
-}
-
-void onConnect(const error_code &err, local::stream_protocol::socket *socket) {
-    if (err) {
-        logger->error(err.message());
-        return;
-    }
-    Cmd cmd;
-    cmd.setCmd(COMMANDS::MM_STOP_MONITOR_MANAGER);
-    async_write(*socket, buffer(cmd.toJson().dump()),
-                std::bind(&onWrite, ph::_1,
-                          ph::_2, socket));
-}
-
 int main() {
     io_context io;
-    init();
-    local::stream_protocol::socket socket(io);
-    socket.async_connect(
-        local::stream_protocol::endpoint(utils::getLocalKekDir() +
-                                         "/sockets/MonitorManager"),
-        std::bind(&onConnect, ph::_1, &socket));
+    kekmonitors::init();
+    auto logger = kekmonitors::utils::getLogger("Stopmm");
+    auto connection = kekmonitors::Connection::create(io);
+    try {
+        connection->socket.connect(local::stream_protocol::endpoint(
+            kekmonitors::utils::getLocalKekDir() + "/sockets/MonitorManager"));
+    } catch (std::exception &) {
+        logger->error("Couldn't connect to socket");
+        return 1;
+    }
+    kekmonitors::Cmd cmd;
+    cmd.setCmd(kekmonitors::COMMANDS::MM_STOP_MONITOR_MANAGER);
+    connection->asyncWriteCmd(
+        cmd, [logger](const kekmonitors::error_code &ec,
+                      kekmonitors::Connection::Ptr connection) {
+            if (ec) {
+                logger->error(ec.message());
+                return;
+            }
+            connection->asyncReadResponse(
+                [logger](const kekmonitors::error_code &ec,
+                         const kekmonitors::Response &resp,
+                         kekmonitors::Connection::Ptr) {
+                    if (ec) {
+                        logger->error(ec.message());
+                        return;
+                    }
+                    kekmonitors::ErrorType e = resp.getError();
+                    if ((e = resp.getError())) {
+                        logger->error(
+                            kekmonitors::utils::errorToString(resp.getError()));
+                        logger->error(resp.getInfo());
+                    } else {
+                        logger->info(
+                            kekmonitors::utils::errorToString(resp.getError()));
+                    }
+                });
+        });
     io.run();
-    socket.close();
 }
